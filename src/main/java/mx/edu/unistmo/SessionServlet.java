183913f1d07a9aacab09bdf5d15833c3616ce34f
Giovanii7u7 <giovaniarango6@gmail.com>
2024-05-13 07:06:38 -0600

    Av. 2-3 – Session-Login

---
+++ src/main/java/mx/edu/unistmo/SessionServlet.java
@@ -0,0 +1,84 @@
+package mx.edu.unistmo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.bson.Document;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+
+import jakarta.servlet.http.Cookie;
+
+@WebServlet("/login")
+public class SessionServlet extends HttpServlet {
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+    private MongoCollection<Document> usuariosCollection;
+    private MongoCollection<Document> passwordsCollection;
+
+    @Override
+    public void init() throws ServletException {
+        mongoClient = MongoClients.create(
+                "mongodb+srv://giovanni:Giovani129se23nf@cluster0.qfbkqre.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        database = mongoClient.getDatabase("miBaseDeDatos");
+        usuariosCollection = database.getCollection("usuarios");
+        passwordsCollection = database.getCollection("passwords");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String usuario = request.getParameter("usuario");
+        String contraseña = request.getParameter("contraseña");
+
+        boolean credencialesValidas = verificarCredenciales(usuario, contraseña);
+
+        if (credencialesValidas) {
+            HttpSession session = request.getSession();
+            session.setAttribute("usuario", usuario);
+
+            if (request.getParameter("recordar") != null) {
+      
+                HttpSession session = request.getSession(true);
+                session.setAttribute("usuario", usuario);
+                session.setMaxInactiveInterval(7 * 24 * 60 * 60); 
+
+                Cookie usuarioCookie = new Cookie("usuario", usuario);
+                Cookie contraseñaCookie = new Cookie("contraseña", contraseña);
+                usuarioCookie.setMaxAge(7 * 24 * 60 * 60); 
+                contraseñaCookie.setMaxAge(7 * 24 * 60 * 60);
+                response.addCookie(usuarioCookie);
+                response.addCookie(contraseñaCookie);
+            }
+
+            response.sendRedirect("bienvenida.jsp");
+        } else {
+            request.setAttribute("mensajeError", "Usuario o contraseña incorrectos");
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+        }
+    }
+
+    private boolean verificarCredenciales(String usuario, String contraseña) {
+
+        Document usuarioDocument = usuariosCollection.find(Filters.eq("usuario", usuario)).first();
+        if (usuarioDocument == null) {
+            return false; // Usuario no encontrado
+        }
+
+        Document contraseñaDocument = passwordsCollection
+                .find(Filters.and(Filters.eq("usuario", usuario), Filters.eq("contraseña", contraseña))).first();
+        return contraseñaDocument != null;
+    }
+
+    @Override
+    public void destroy() {
+        mongoClient.close();
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
 <html>
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+    <h2>Login</h2>
+    <form action="login" method="post">
+        Usuario: <input type="text" name="usuario"><br>
+        Contraseña: <input type="password" name="contraseña"><br>
+        <input type="checkbox" name="recordar" value="recordar"> Recordar credenciales<br>
+        <input type="submit" value="Iniciar sesión">
+    </form>
 </body>
 </html>
